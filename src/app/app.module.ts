46d9cea13fc0fafe53f88b255eeb174350220fb6
Ingeborg Slegers <ingeborgfreda@gmail.com>
2019-03-19 11:04:06 -0400

    created sw-service and star-wars component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CupsComponent } from './cups/cups.component';
+import { StarWarsComponent } from './star-wars/star-wars.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CupsComponent,
+    StarWarsComponent
   ],
   imports: [
     BrowserModule
